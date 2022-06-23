de1a1b99a8be1046116686fdfe5145b62c53347f
Jieyu <nicolewang404@outlook.com>
2022-06-23 21:35:00 +0800

    answer for flaten exercise

--- src/main/java/com/thoughtworks/collection/Flaten.java
+++ src/main/java/com/thoughtworks/collection/Flaten.java
@@ -1,21 +1,31 @@
 package com.thoughtworks.collection;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Flaten {
 
     Integer[][] array;
+
     public Flaten(Integer[][] array) {
         this.array = array;
     }
 
     public List<Integer> transformToOneDimesional() {
-        throw new NotImplementedException();
+//        for (int i = 0; i < array.length; i++) {
+//            Integer[] result = (Integer[]) array[i];
+//           // for (Integer)
+//        }
+//      return null;
+        List<Integer> resultList = new ArrayList<>();
+        //s.forEach(resultList::addAll);
+        return resultList;
+
+
     }
 
     public List<Integer> transformToUnrepeatedOneDimesional() {
-        throw new NotImplementedException();
+        return null;
     }
 }
