32062270a7aac03e9536715e0d40d86c32958d40
Jieyu <nicolewang404@outlook.com>
2022-06-23 21:36:23 +0800

    answer for InverseReduce exercise

--- src/main/java/com/thoughtworks/collection/InverseReduce.java
+++ src/main/java/com/thoughtworks/collection/InverseReduce.java
@@ -1,6 +1,5 @@
 package com.thoughtworks.collection;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.List;
 import java.util.Random;
@@ -16,6 +15,7 @@ public class InverseReduce {
     }
 
     public List<Integer> divideToSmaller(int number) {
-        throw new NotImplementedException();
+
+        return null;
     }
 }
